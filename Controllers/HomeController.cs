ee180fdf2720b7e429cb8da8703cdf19d77ef7c9
Git <ngominhdaicd@gmail.com>
2022-11-15 11:02:02 +0700

    D

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using ChiaSeDuLich.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChiaSeDuLich.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TravelShareDBContext db = new TravelShareDBContext();
+        public ActionResult Index()
+        {
+            LoginController.isLogin = true;
+            List<InforShare> listIf = db.InforShares.ToList();
+            listIf.ForEach(e =>
+            {
+                List<Comment> listCms = db.Comments.Where(x => x.InforShareId == e.InforShareId).ToList();
+                e.OwnComments = new List<Comment>();
+                if (listCms != null)
+                {
+                    e.OwnComments.AddRange(listCms);
+                }
+                List<Rank> listRanks = db.Ranks.Where(x => x.InforShareId == e.InforShareId).ToList();
+                e.RankAve = 0;
+                if (listRanks?.Count > 0)
+                {
+                    e.RankAve = listRanks.Sum(x => x.Value) / listRanks.Count;
+                }
+            });
+            return View(listIf);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/InforShare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ChiaSeDuLich.Models
+{
+    [Table("InforShare")]
+    public class InforShare
+    {
+        [Key]
+        public int InforShareId { get; set; }
+        [DisplayName("Tiêu đề chia sẻ")]
+        public string InforShareTitle { get; set; }
+        [DisplayName("Nội dung chia sẻ")]
+        public string InforShareContent { get; set; }
+        [DisplayName("Nguồn chia sẻ")]
+        public string InforShareSource { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public List<Comment> OwnComments { get; set; }
+        [NotMapped]
+        public decimal RankAve { get; set; } = 0;
+    }
+}
