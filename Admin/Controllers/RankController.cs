8dd5c1d5247f19c86b99631452b78efbc84edb64
Thanhbo1901 <117528236+Thanhbo1901@users.noreply.github.com>
2022-11-20 20:53:37 +0700

    Quản lý bình luận và đánh giá
    Quản lý bình luận và đánh giá

---
+++ Admin/Controllers/RankController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChiaSeDuLich.Models;
+
+namespace ChiaSeDuLich.Areas.Admin.Controllers
+{
+    public class RankController : Controller
+    {
+        private TravelShareDBContext db = new TravelShareDBContext();
+
+        // GET: Admin/Rank
+        public ActionResult Index()
+        {
+            return View(db.Ranks.ToList());
+        }
+
+        // GET: Admin/Rank/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rank rank = db.Ranks.Find(id);
+            if (rank == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rank);
+        }
+
+        // GET: Admin/Rank/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Rank/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RankId,InforShareId,UserId,UserName,Value")] Rank rank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ranks.Add(rank);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rank);
+        }
+        // POST: Admin/Rank/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public bool CreateRank(Rank rank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ranks.Add(rank);
+                db.SaveChanges();
+            }
+
+            return true;
+        }
+
+        // GET: Admin/Rank/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rank rank = db.Ranks.Find(id);
+            if (rank == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rank);
+        }
+
+        // POST: Admin/Rank/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RankId,InforShareId,UserId,UserName,Value")] Rank rank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rank).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rank);
+        }
+
+        // GET: Admin/Rank/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rank rank = db.Ranks.Find(id);
+            if (rank == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rank);
+        }
+
+        // POST: Admin/Rank/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Rank rank = db.Ranks.Find(id);
+            db.Ranks.Remove(rank);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
